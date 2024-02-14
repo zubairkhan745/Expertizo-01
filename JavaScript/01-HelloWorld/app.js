6e025b34cf5e6b45c587edac78306fc485183da6
unknown <kb2423615@gmail.com>
2024-02-14 17:19:49 +0500

    Expertizo

---
+++ JavaScript/01-HelloWorld/app.js
@@ -0,0 +1,28 @@
+console.log("Hello World Js");
+
+// alert("Hello World");
+
+//data Type
+
+//String
+//number
+//boolean
+
+//baad me parhenge
+//object
+
+// var firstname = "Zubair";
+// var age = 21;
+// var boy = true;
+
+// console.log(typeof(firstname));
+// console.log(typeof(age));
+// console.log(typeof(boy));
+
+
+var firstname = prompt("Enter First Name");
+var lastname = prompt("Enter Last Name");
+var age = prompt("Enter Your age");
+
+console.log("Hello " + firstname + " " +lastname + " you are " + age + " Years Old.");
+
